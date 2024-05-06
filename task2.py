a75d3cca450ea0df4e5b47dae0faf21aa6eddaa7
Natalia Kucheriava <117598305+Nata-Curly@users.noreply.github.com>
2024-05-06 22:39:58 +0200

    hw done

---
+++ task2.py
@@ -0,0 +1,45 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    return merge(merge_sort(left_half), merge_sort(right_half))
+
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    while left_index < len(left):
+        merged.append(left[left_index])
+        left_index += 1
+
+    while right_index < len(right):
+        merged.append(right[right_index])
+        right_index += 1
+
+    return merged
+
+
+def merge_k_lists(lists):
+    merged_lists = []
+    for i in range(len(lists)):
+        merged_lists = merge(merged_lists, lists[i])
+    return merged_lists
+
+
+lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
+merged_list = merge_k_lists(lists)
+print("Відсортований список:", merged_list)
